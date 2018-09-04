dd4ac518589da59b0c3754fadfb483c33743ed54
dragon4147 <dragonlyj@gmail.com>
2018-09-04 11:03:28 +0800

    add

--- Lean/Unification.lean
+++ Lean/Unification.lean
@@ -4,7 +4,27 @@ inductive Fin : nat -> Type
 | f0 : Fin (nat.succ nat.zero)
 | fs {n: nat} : Fin n -> Fin (nat.succ n)
 
-#print Fin
+#check @Fin.rec
+--#print Fin.rec
+#check @Fin.rec_on
+--#print fin.rec_on
+#check @Fin.below
+#check @Fin.ibelow
+#check @Fin.brec_on
+#check @Fin.binduction_on
+#check @Fin.cases_on
+#check @Fin.sizeof
+--#print Fin.sizeof
+--#reduce (Fin.sizeof _ (Fin.fs (Fin.fs Fin.f0)))
+#check @Fin.has_sizeof_inst
+#check @Fin.no_confusion
+#check @Fin.no_confusion_type
+#check @Fin.fs.inj
+#check @Fin.fs.inj_arrow
+#check @Fin.fs.inj_eq
+#check @Fin.fs.sizeof_spec
+--#check @well_founded.fix_F
+--#check @well_founded.fix_F_eq
 
 inductive Term : nat -> Type
 | leaf {n:nat} : Term n
@@ -12,7 +32,8 @@ inductive Term : nat -> Type
 | fork {n:nat} (s: Term n)(t:Term n) : Term n
 
 notation `ι` := Term.var
-#print Term
+--#print Term.rec
+--#print Term.rec_on
 
 /-- ▸ ◂ -/
 
@@ -44,21 +65,21 @@ notation f `◇` g := subCompose f g
 
 #print subCompose
 
-lemma subIotaId {m:nat} (t: Term m): ι ◃ t = t
+def subIotaId {m:nat} : Π(t: Term m), ι ◃ t = t
 := sorry
 /-
 | Term.leaf := refl
 | (Term.var n) := refl
-| (Term.fork s t) := sorry
+| (Term.fork s t) := refl
 -/
 #check @subIotaId
 
-lemma subComposeLiftFlat {m n l:nat}(t:Term l)(f:Fin m -> Term n)(g:Fin l -> Term m):
+def subComposeLiftFlat {m n l:nat}(t:Term l)(f:Fin m -> Term n)(g:Fin l -> Term m):
 (f ◇ g) ◃ t = f ◃ (g ◃ t)
 := sorry
 #check @subComposeLiftFlat
 
-lemma flatSubCompose {m n l:nat}(f:Fin n -> Term l)(r: Fin m -> Fin n):
+def flatSubCompose {m n l:nat}(f:Fin n -> Term l)(r: Fin m -> Fin n):
 (f ◇ (▹ r)) = λ k, f (r k)
 := sorry
 #check @flatSubCompose
--- Lean/tutorial.lean
+++ Lean/tutorial.lean
@@ -1,35 +1,70 @@
-universes u v
-variables {α : Type u} {β : Type v}
+/- basic induction principle
+rec
+rec_on
+constructor.inj
+constructor.eq
+-/
 
-def p : ℕ × ℤ := ⟨1, 2⟩
-#check p.fst
-#check p.snd
+/- basic proof tatic
+axiom <-> constant (postulate)
+assume <-> lambda
+show , from <-> term with anotated type
+have,from  <-> let in
+suffices from <-> beta reduction: (lambda subgoal -> global goal) subgoal
+-/
 
-def p' : ℕ × ℤ × bool := ⟨1, 2, tt⟩
-#check p'.fst
-#check p'.snd.fst
-#check p'.snd.snd
+variables p q : Prop
 
-def swap_pair (p : α × β) : β × α :=
-⟨p.snd, p.fst⟩
+example (h : p ∨ q) : q ∨ p :=
+h.elim
+  (assume hp : p,
+    show q ∨ p, from or.intro_right q hp)
+  (assume hq : q,
+    show q ∨ p, from or.intro_left p hq)
 
-theorem swap_conj {a b : Prop} (h : a ∧ b) : b ∧ a :=
-⟨h.right, h.left⟩
+example : p ∨ q -> q ∨ p
+| (or.inl hp) := or.inr hp
+| (or.inr hq) := or.inl hq
 
-#check [1, 2, 3].append [2, 3, 4]
-#check [1, 2, 3].map (λ x, x^2)
+example (hp : p) (hnp : ¬p) : q := false.elim (hnp hp)
 
-example (p q : Prop) : p ∧ q → q ∧ p :=
-λ h, ⟨h.right, h.left⟩
+example (hp : p) (hnp : ¬p) : q := absurd hp hnp
 
-def swap_pair' (p : α × β) : β × α :=
-let (x, y) := p in (y, x)
+#check @true.rec
 
-theorem swap_conj' {a b : Prop} (h : a ∧ b) : b ∧ a :=
-let ⟨ha, hb⟩ := h in ⟨hb, ha⟩
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+iff.intro
+  (assume h : p ∧ q,
+    show q ∧ p, from and.intro (and.right h) (and.left h))
+  (assume h : q ∧ p,
+    show p ∧ q, from and.intro (and.right h) (and.left h))
 
-def swap_pair'' : α × β → β × α :=
-λ ⟨x, y⟩, (y, x)
+#check and_swap p q    -- p ∧ q ↔ q ∧ p
 
-theorem swap_conj'' {a b : Prop} : a ∧ b → b ∧ a :=
-assume ⟨ha, hb⟩, ⟨hb, ha⟩
+theorem and_swap1 : p ∧ q ↔ q ∧ p :=
+⟨ assume h, ⟨h.right, h.left⟩,
+ λ h, ⟨h.right, h.left⟩ ⟩
+
+example (h : p ∧ q) : q ∧ p := (and_swap p q).mp h
+
+example (h : p ∧ q) : q ∧ p :=
+have hp : p, from and.left h,
+have hq : q, from and.right h,
+show q ∧ p, from and.intro hq hp
+
+
+section
+open classical
+
+#check em p
+
+example (h : ¬¬p) : p :=
+by_cases
+  (assume h1 : p, h1)
+  (assume h1 : ¬p, absurd h1 h)
+
+theorem dne {p : Prop} (h : ¬¬p) : p :=
+or.elim (em p)
+  (assume hp : p, hp)
+  (assume hnp : ¬p, absurd hnp h)
+end
